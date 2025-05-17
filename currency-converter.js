3d4f1556a4e2f4e5d1f7bd9ec5ed68c84a3c63b2
ismail-dev-code <hm.ismail772@gmail.com>
2025-05-17 22:35:37 +0600

    feat: functionality implemented

--- currency-converter.js
+++ currency-converter.js
@@ -1,8 +1,31 @@
 class currencyConverter {
   async setBaseCurrency(currency) {
     const apiUrl = `https://api.exchangerate-api.com/v4/latest/${currency.toUpperCase()}`;
-    const response = fetch(apiUrl);
-    const data = (await response).json();
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error("cannot fetch exchange rate.");
+    }
+    const data = await response.json();
+    this.rates = data.rates;
   }
-  async convertTo(currency, amount = 1) {}
+  async convertTo(currency, amount = 1) {
+    const _currency = currency.toUpperCase();
+    if (!(_currency in this.rates)) {
+      throw new Error("currency not found.");
+    }
+    const result = (this.rates[_currency] * amount).toFixed(2);
+    return result;
+  }
 }
+(async () => {
+  const cc = new currencyConverter();
+  await cc.setBaseCurrency("usd");
+  const bdt = await cc.convertTo("bdt");
+  console.log(bdt);
+  const aud = await cc.convertTo("aud", 100);
+  console.log(aud);
+
+  await cc.setBaseCurrency("eur");
+  const bdtAgain = await cc.convertTo("bdt");
+  console.log(bdtAgain);
+})();
